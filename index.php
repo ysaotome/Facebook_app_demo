6114e2dbb34a4ca4bafa5099a13e2159912c5250
Okawa Yuki <tb303.skyfish@gmail.com>
2012-09-06 07:35:12 +0900

    Update index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once 'model.php';
 
 $uri = $_SERVER['REQUEST_URI'];
-if ($uri == '/index.php' || $uri == '/' || isset($_GET['state']))) {
+if ($uri == '/index.php' || $uri == '/' || isset($_GET['state'])) {
     list_friends();
 } elseif ($uri == '/index.php' && isset($_GET['src_id']) && isset($_GET['dest_id'])) {
     http_to_friends($_GET['src_id'], $_GET['dest_id']);
