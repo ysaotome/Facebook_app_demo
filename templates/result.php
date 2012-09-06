fa76c16493c7f1c04068729bcdc1ad026d5a3946
OkawaUki <okawa.yuki@nifty.co.jp>
2012-09-06 12:49:25 +0900

    Update result.php

--- templates/result.php
+++ templates/result.php
@@ -8,7 +8,7 @@
 	<body>
     	<h1>HTTP ACCESS TO FACEBOOK FRIENDS</h1>
 		<p>
-			You Access to <img src="https://graph.facebook.com/<?php echo $GLOBALS['dest_id'] ?>/picture" alt="<?php echo $GLOBALS['dest_id'] ?> Profile Photo"><br>
+			You Access to <img src="https://graph.facebook.com/<?php echo $dest ?>/picture" alt="<?php echo $dest ?> Profile Photo"><br>
 			<?php echo $result['cord'].':'.$result['status'] ?>
 		</p>
 	</body>
